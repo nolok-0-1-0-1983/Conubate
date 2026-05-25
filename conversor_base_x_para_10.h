9f700497b09a19316060d80939f872eb38da09ef
jmpdf <92542261+jmpdf@users.noreply.github.com>
2026-05-25 06:50:08 -0300

    Update conversor_base_x_para_10.h

--- conversor_base_x_para_10.h
+++ conversor_base_x_para_10.h
@@ -1,10 +1,10 @@
 #ifndef CONVERSOR_BASE_X_PARA_10
 #define CONVERSOR_BASE_X_PARA_10
 
-std::string fracionario_de_base_x_para_10(std::string num_x, short base_x);
+std::string fracionario_de_base_x_para_10(std::string num_x, short base_x, bool passo_a_passo);
 
-std::string inteiro_de_base_x_para_10(std::string num_x, short base_x);
+std::string inteiro_de_base_x_para_10(std::string num_x, short base_x, bool passo_a_passo);
 
-std::string de_base_x_para_10(std::string num_x, short base_x);
+std::string de_base_x_para_10(std::string num_x, short base_x, bool passo_a_passo);
 
 #endif
