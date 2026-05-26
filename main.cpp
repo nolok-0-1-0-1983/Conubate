2796e7fb7618473971ed76b7e6793a17e4cf0824
jmpdf <92542261+jmpdf@users.noreply.github.com>
2026-05-26 12:08:34 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "verificador.h"
 #include "quiz.h"
 #include "leitor_csv.h"
+#include "calculadora_maximos.h"
 
 using namespace std;
 
@@ -13,7 +14,7 @@ int main(){
 	bool repetir = true;
 	string resposta;
 	do{
-		cout << "1 - converter numero\n2 - ler aquivo csv\n3 - modo quiz\n4 - sair\n";
+		cout << "1 - converter numero\n2 - ler arquivo csv\n3 - modo quiz\n4 - calculadora de maximos\n5 - sair\n";
 		cin >> resposta;
 
 		if(resposta == "1"){
@@ -23,20 +24,28 @@ int main(){
 			cout << "insira o nome do arquivo\n";
 			cin >> resposta;
 			ifstream entrada(resposta);
-			ler_e_converter_csv(&entrada);
-			cout << "arquivo 'saida.csv' foi gerado\n";
-
+			if(entrada.good()){
+				ler_e_converter_csv(&entrada);
+				cout << "arquivo 'saida.csv' foi gerado\n";
+			}else{
+				cout << "O arquivo nao existe\n";
+			}
 		}else if(resposta == "3"){
 			bool continuar_quiz = true;
 			int dificuldade = 1;
 			while(continuar_quiz){
-				cout << "sua pontuação total no quiz é " << pontuacao_quiz << ", aperte p para gerar uma nova pergunta ou q para sair\n";
+				cout << "sua pontuação total no quiz é " << pontuacao_quiz << ", a dificuladade atual é " << dificuldade << ".\n1 - gerar nova pergunta\n2 - aumentar dificuldade\n3 - diminuir dificuldade\n4 - sair\n";
 				cin >> resposta;
-				if(resposta == "q"){continuar_quiz = false;}else
-				if(resposta == "p"){pontuacao_quiz += gerar_conversao(dificuldade);} 
+				if(resposta == "4"){continuar_quiz = false;}else
+				if(resposta == "1"){pontuacao_quiz += gerar_conversao(dificuldade);}else
+				if(resposta == "2"){if(dificuldade < 5){dificuldade++;}}else
+				if(resposta == "3"){if(dificuldade > 1){dificuldade--;}}else
+				{cout << "resposta invalida\n";}
 			}
 
 		}else if(resposta == "4"){
+			calcular_maximos();
+		}else if(resposta == "5"){
 			repetir = false;
 		}else{
 			cout << "resposta invalida, tente novamente\n";
